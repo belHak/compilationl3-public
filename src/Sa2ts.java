0f5de541b82526aac5e4f04e3e38a37b57752c30
Smeunzo <fsl12@hotmail.fr>
2020-02-25 00:24:47 +0100

    TP3 TEST PASSE À 100%
    Ajout d'un context et de verification lors des déclarations.
    
    Ajout d'une nested class "Throw" qui gère les exceptions.
    
    Reformat de code.

--- src/Sa2ts.java
+++ src/Sa2ts.java
@@ -7,12 +7,18 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     private Ts tsGlobal = new Ts();
     private Ts currentLocalTable;
 
+    private enum Context {GLOBAL, ARGUMENT, VARIABLE}
+
+    ;
+    private Context context;
+
     Sa2ts(SaNode root) {
+        context = Context.GLOBAL;
         root.accept(this);
-        if(FncDoesNotExists("main")) throw new RuntimeException("main function must be declared ");
-        if(tsGlobal.getFct("main").nbArgs > 0){
-            throw new RuntimeException("main function must not have arguments");
-        }
+        context = Context.GLOBAL;
+
+        if (FncDoesNotExists("main")) Throw.exception("main function must be declared ");
+        if (tsGlobal.getFct("main").nbArgs > 0) Throw.exception("main function must not have arguments");
     }
 
     public Ts getTableGlobale() {
@@ -22,13 +28,16 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     @Override
     public Void visit(SaDecTab node) {
         defaultIn(node);
-        String nom = node.getNom();
-        //Should throw Exception;
-        if (varExists(nom, tsGlobal)) return null;
+        if (context != Context.GLOBAL) Throw.exception("Array should be declared as global");
 
+        String nom = node.getNom();
         int taille = node.getTaille();
-        if (taille < 2) throw new IllegalArgumentException("array size must be greater than or equal to 2");
+
+        if (varExists(nom, tsGlobal)) Throw.exception("This variable already exists");
+        if (taille < 2) Throw.exception("Array size must be greater than or equal to 2");
+
         node.tsItem = tsGlobal.addVar(nom, taille);
+
         defaultOut(node);
         return null;
     }
@@ -36,17 +45,29 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     @Override
     public Void visit(SaDecFonc node) {
         defaultIn(node);
+        Ts localTable = new Ts();
+        currentLocalTable = localTable;
 
-        //Throw RuntimeException to avoid declaring it explicitly in the method
-        if (FncExists(node.getNom())) throw new RuntimeException("Fonction " + node.getNom() +
+        String nom = node.getNom();
+        int nbArgs = node.getParametres() == null ? 0 : node.getParametres().length();
+
+        context = Context.GLOBAL;
+
+        if (FncExists(node.getNom())) Throw.exception("Fonction " + node.getNom() +
                 " is already defined");
 
-        //Returns boolean but should return void
-        if (!addFunctionToSymbolTable(node)) return null;
+        context = Context.ARGUMENT;
+        if (node.getParametres() != null) node.getParametres().accept(this);
 
+        context = Context.VARIABLE;
+        if (node.getVariable() != null) node.getVariable().accept(this);
+
+        if (node.getCorps() != null) node.getCorps().accept(this);
+
+        context = Context.GLOBAL;
         currentLocalTable = tsGlobal.getTableLocale(node.getNom());
-        node.getCorps().accept(this);
 
+        node.tsItem = tsGlobal.addFct(nom, nbArgs, localTable, node);
         defaultOut(node);
         return null;
     }
@@ -55,10 +76,19 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     @Override
     public Void visit(SaDecVar node) {
         defaultIn(node);
-        if (varExists(node.getNom(), tsGlobal)) {
-            throwVarDecTwice(node.getNom());
+        switch (context) {
+            case GLOBAL:
+                declareVarGlobally(node);
+                break;
+            case ARGUMENT:
+                declareParamLocally(node);
+                break;
+            case VARIABLE:
+                declareVarLocally(node);
+                break;
+            default:
+                Throw.exception("Unknown Context type");
         }
-        node.tsItem = tsGlobal.addVar(node.getNom(), 1);
         defaultOut(node);
         return null;
     }
@@ -67,13 +97,13 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     @Override
     public Void visit(SaVarSimple node) {
         defaultIn(node);
-        //doit vérifier si la variable existe dans cette portée
 
+        //This flag is independent of the context
         boolean isGlobal = false;
         if (varDoesNotExists(node.getNom(), currentLocalTable)) {
             if (varDoesNotExists(node.getNom(), tsGlobal)) {
                 //Kids should not be allowed to see that
-                 throwVarNotDeclared(node.getNom());
+                throwVarNotDeclared(node.getNom());
             } else {
                 isGlobal = true;
             }
@@ -87,14 +117,13 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         }
 
         if (tsItemVar.taille > 1) {
-            throw new RuntimeException("Bad utilisation of array");
+            Throw.exception("Bad utilisation of array");
         }
 
         node.tsItem = tsItemVar;
         defaultOut(node);
         return null;
     }
-
 
     @Override
     public Void visit(SaAppel node) {
@@ -104,8 +133,9 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         int nbARgs = node.getArguments() == null ? 0 : node.getArguments().length();
         TsItemFct tsItemFct = tsGlobal.getFct(nom);
 
-        if(FncDoesNotExists(nom)) throw new RuntimeException("This function : "+nom+" does not exists");
-        if(nbARgs != tsItemFct.nbArgs) throw new RuntimeException("Expected "+tsItemFct.nbArgs+" argument(s) but got "+nbARgs);
+        if (FncDoesNotExists(nom)) Throw.exception("This function : " + nom + " does not exists");
+        if (nbARgs != tsItemFct.nbArgs)
+            Throw.exception("Expected " + tsItemFct.nbArgs + " argument(s) but got " + nbARgs);
 
         node.tsItem = tsItemFct;
         defaultOut(node);
@@ -113,29 +143,20 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         return null;
     }
 
-    private boolean FncDoesNotExists(String nom) {
-        return !FncExists(nom);
-    }
-
     @Override
     public Void visit(SaVarIndicee node) {
         defaultIn(node);
 
-        if (varExists(node.getNom(), currentLocalTable))
-            throw new RuntimeException(node.getNom() + " is a local variable not a global array");
-        if (varDoesNotExists(node.getNom(), tsGlobal))
-             throwVarNotDeclared(node.getNom());
+        if (varExists(node.getNom(), currentLocalTable)) Throw.exception(node.getNom() +
+                " is a local variable not a global array");
+        if (varDoesNotExists(node.getNom(), tsGlobal)) throwVarNotDeclared(node.getNom());
 
-        if (tsGlobal.getVar(node.getNom()).taille < 2) {
-            throw new RuntimeException("this is not an array");
-        }
+        if (tsGlobal.getVar(node.getNom()).taille < 2) Throw.exception("Variable used as a variable");
         node.tsItem = tsGlobal.getVar(node.getNom());
 
         defaultOut(node);
         return null;
     }
-
-    private int indentation = 0;
 
     @Override
     public void defaultIn(SaNode node) {
@@ -155,54 +176,17 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
         System.out.println("</" + node.getClass().getSimpleName() + ">");
     }
 
-    private boolean addFunctionToSymbolTable(SaDecFonc node) {
-        Ts localTable = new Ts();
-        String nom = node.getNom();
+/***********************************************************************
+ ***********************************************************************
+ ***************************MÉTHODES PRIVÉES****************************
+ ***********************************************************************
+ ***********************************************************************/
 
-        int nbArgs = node.getParametres() == null ? 0 : node.getParametres().length();
-        int nbVar = node.getVariable() == null ? 0 : node.getVariable().length();
-
-        SaLDec varIterateur = node.getVariable();
-        SaDec teteVarList = nbVar == 0 ? null : varIterateur.getTete();
-        for (int i = 0; i < nbVar; i++) {
-
-
-            if (varExists(teteVarList.getNom(), localTable))
-                throwVarDecTwice(teteVarList.getNom());
-            if (teteVarList instanceof SaDecTab)
-                throwNotGlobalArray();
-
-            localTable.addVar(teteVarList.getNom(), 1);
-
-            if (varIterateur.getQueue() == null) break;
-            varIterateur = varIterateur.getQueue();
-            teteVarList = varIterateur.getTete();
-        }
-
-        SaLDec argIterateur = node.getParametres();
-        SaDec teteParamList = nbArgs == 0 ? null : argIterateur.getTete();
-        for (int i = 0; i < nbArgs; i++) {
-
-            if (varExists(teteParamList.getNom(), localTable))
-                throwVarDecTwice(teteParamList.getNom());
-            if (teteParamList instanceof SaDecTab) {
-                 throwNotGlobalArray();
-            }
-
-            localTable.addParam(teteParamList.getNom());
-
-            if (argIterateur.getQueue() == null) break;
-            argIterateur = argIterateur.getQueue();
-            teteParamList = argIterateur.getTete();
-        }
-
-        node.tsItem = tsGlobal.addFct(nom, nbArgs, localTable, node);
-        return true;
+    private boolean FncDoesNotExists(String nom) {
+        return !FncExists(nom);
     }
 
-    private void throwNotGlobalArray() {
-        throw new IllegalArgumentException("Arrays should be declared as global variables");
-    }
+    private int indentation = 0;
 
 
     private boolean varExists(String nom, Ts table) {
@@ -218,10 +202,35 @@ public class Sa2ts extends SaDepthFirstVisitor<Void> {
     }
 
     private void throwVarNotDeclared(String varName) {
-        throw new RuntimeException("var : " + varName + " has not been declared");
+        Throw.exception("var : " + varName + " has not been declared");
     }
 
     private void throwVarDecTwice(String nom) {
-        throw new IllegalArgumentException("var : " + nom + " is declared twice");
+        Throw.exception("var : " + nom + " is declared twice");
+    }
+
+    private void declareParamLocally(SaDecVar node) {
+        if (varExists(node.getNom(), currentLocalTable)) throwVarDecTwice(node.getNom());
+        node.tsItem = currentLocalTable.addParam(node.getNom());
+    }
+
+    private void declareVarLocally(SaDecVar node) {
+        if (varExists(node.getNom(), currentLocalTable)) throwVarDecTwice(node.getNom());
+        node.tsItem = currentLocalTable.addVar(node.getNom(), 1);
+    }
+
+    private void declareVarGlobally(SaDecVar node) {
+        if (varExists(node.getNom(), tsGlobal)) throwVarDecTwice(node.getNom());
+        node.tsItem = tsGlobal.addVar(node.getNom(), 1);
+    }
+
+    private static class Throw extends RuntimeException {
+        private Throw(String message) {
+            super(message);
+        }
+
+        public static void exception(String message) {
+            throw new Throw(message);
+        }
     }
 }
